904296f02e863457c3da848f98b78e6c93a18017
Maginor84 <magnus.dahler.norling@gmail.com>
2023-06-12 11:13:58 +0200

    Don't plot all_to_all fluxes from an index to itself (since these are also no longer computed)

--- MyPlot.cpp
+++ MyPlot.cpp
@@ -161,6 +161,8 @@ bool add_plot_recursive(MyPlot *draw, Model_Application *app, Var_Id var_id, std
 				indexes2[idx] = indexes[index_sets[idx].id];
 			int count = app->get_index_count(set, indexes).index;
 			for(int idx = 0; idx < count; ++idx) {
+				if(indexes2[sz-2].index == idx) continue; // Skip for when the two last indexes are the same.
+				
 				indexes2[sz-1].index = idx;
 				Color &graph_color = draw->colors.next();
 				bool success = add_single_plot(draw, &app->data, app, var_id, indexes2, time_steps,
