b045e335f2badc318a6cc266ccbed8ee13a64eac
Magnus Dahler Norling <magnus.dahler.norling@gmail.com>
2024-10-15 14:40:10 +0200

    Fix bug

--- MyPlot.cpp
+++ MyPlot.cpp
@@ -634,7 +634,8 @@ format_axes(MyPlot *plot, Plot_Mode mode, int n_bins_histogram, Date_Time input_
 				plot->SetRange((double)count, Null);
 				plot->cbModifFormatX << [count, plot](String &s, int i, double d) {
 					int idx = (int)std::floor(d);
-					if(idx >= -1 && idx <= count-1 && (idx < plot->labels.size())) s = plot->labels[idx+1];
+					if(idx >= -1 && idx <= count-1 && (idx+1 < plot->labels.size())) 
+						s = plot->labels[idx+1];
 					else s = "";
 				};
 			}
